9fc9cfba39db68ef57f1fd96b43e3cd8fb2d509b
Ashir-Naeem <coolashir786@gmail.com>
2024-12-14 23:09:42 +0500

    first commit

--- src/component/Home.tsx
+++ src/component/Home.tsx
@@ -12,7 +12,7 @@ const Home = () => {
         <h1 style={{ fontSize: "50px" }}>Ashir Naeem</h1>
         <Type />
         <p>
-        Hi, I'm Ashir Naeem, a dedicated frontend developer specializing in HTML, CSS, JavaScript, and Next.js. I focus on building scalable, dynamic, and responsive web applications using modern frameworks and libraries. With expertise in frontend technologies such as HTML, CSS, JavaScript, and Next.js, I create robust, user-centric interfaces that ensure seamless and engaging user experiences. My goal is to combine functionality with clean, efficient code to deliver powerful web solutions.
+        Hi, Im Ashir Naeem, a dedicated frontend developer specializing in HTML, CSS, JavaScript, and Next.js. I focus on building scalable, dynamic, and responsive web applications using modern frameworks and libraries. With expertise in frontend technologies such as HTML, CSS, JavaScript, and Next.js, I create robust, user-centric interfaces that ensure seamless and engaging user experiences. My goal is to combine functionality with clean, efficient code to deliver powerful web solutions.
         </p>
         <button className="btn">
           <Link
